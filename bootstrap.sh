91f00cff4fe4a5f207b4eefc3dce13c704971a51
Beardsley <mibeard@amazon.com>
2019-08-28 16:29:40 -0700

    Updated repo URL for testing

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,15 @@
-N Virginia
-Ohio
-Oregon
-Ireland
-Frankfurt
-Singapore
-Tokyo
+#N Virginia
+#Ohio
+#Oregon
+#Ireland
+#Frankfurt
+#Singapore
+#Tokyo
 
 git config --global credential.helper '!aws codecommit credential-helper $@'
 git config --global credential.UseHttpPath true
 
-git clone --mirror https://github.com/aws-samples/aws-saas-factory-postgresql-rls.git github-dist
+git clone --mirror https://github.com/brtrvn/srvls.git github-dist
 cd github-dist
 git push https://git-codecommit.us-west-2.amazonaws.com/v1/repos/saas-factory-serverless-workshop --all
 cd ..
