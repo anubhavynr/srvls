9b94b7cc19e03da2e0ca7d97dc52b67d033547e5
Beardsley <mibeard@amazon.com>
2019-09-15 15:11:55 -0700

    Specifying full path to PID file

--- monolith/src/main/java/com/amazon/aws/partners/saasfactory/ServerlessSaaSWorkshop.java
+++ monolith/src/main/java/com/amazon/aws/partners/saasfactory/ServerlessSaaSWorkshop.java
@@ -1,10 +1,11 @@
 package com.amazon.aws.partners.saasfactory;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.ApplicationPidFileWriter;
+
+import java.io.File;
 
 @SpringBootApplication
 public class ServerlessSaaSWorkshop {
@@ -12,9 +13,8 @@ public class ServerlessSaaSWorkshop {
 	public static void main(String[] args) {
 		SpringApplicationBuilder app = new SpringApplicationBuilder(ServerlessSaaSWorkshop.class)
 				.web(WebApplicationType.SERVLET);
-		app.build().addListeners(new ApplicationPidFileWriter("monolith.pid"));
+		app.build().addListeners(new ApplicationPidFileWriter(new File("/home/ec2-user/monolith.pid")));
 		app.run();
-//		SpringApplication.run(ServerlessSaaSWorkshop.class, args);
 	}
 
 }
