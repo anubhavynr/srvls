5143a16595d773cb8139d49d226b27250be6ad91
Beardsley <mibeard@amazon.com>
2019-09-16 18:08:05 -0700

    Making the products view dynamic

--- monolith/src/main/java/com/amazon/aws/partners/saasfactory/service/ProductService.java
+++ monolith/src/main/java/com/amazon/aws/partners/saasfactory/service/ProductService.java
@@ -1,11 +1,10 @@
 package com.amazon.aws.partners.saasfactory.service;
 
 import com.amazon.aws.partners.saasfactory.domain.Product;
-import com.amazon.aws.partners.saasfactory.domain.User;
 
 import java.util.List;
 
 public interface ProductService {
 
-    public List<Product> getProductsByUser(User user) throws Exception;
+    public List<Product> getProducts() throws Exception;
 }
--- monolith/src/main/java/com/amazon/aws/partners/saasfactory/service/ProductServiceImpl.java
+++ monolith/src/main/java/com/amazon/aws/partners/saasfactory/service/ProductServiceImpl.java
@@ -1,21 +1,30 @@
 package com.amazon.aws.partners.saasfactory.service;
 
 import com.amazon.aws.partners.saasfactory.domain.Product;
-import com.amazon.aws.partners.saasfactory.domain.User;
 import com.amazon.aws.partners.saasfactory.repository.ProductDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StopWatch;
 
 import java.util.List;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
+    private static final Logger logger = LoggerFactory.getLogger(ProductServiceImpl.class);
+
     @Autowired
     private ProductDao productDao;
 
     @Override
-    public List<Product> getProductsByUser(User user) throws Exception {
-        return productDao.getProducts();
+    public List<Product> getProducts() throws Exception {
+        StopWatch timer = new StopWatch();
+        timer.start();
+        List<Product> products = productDao.getProducts();
+        timer.stop();
+        logger.info("getProducts execution " + timer.getTotalTimeMillis());
+        return products;
     }
 }
