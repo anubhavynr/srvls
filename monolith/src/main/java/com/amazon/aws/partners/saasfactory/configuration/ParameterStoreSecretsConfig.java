c3b328e4c23620f4e16a9aa2dbbd52e849716c82
Beardsley <mibeard@amazon.com>
2019-09-18 09:47:35 -0700

    Adding parameter store secrets config to get the database password from the vault on boot

---
+++ monolith/src/main/java/com/amazon/aws/partners/saasfactory/configuration/ParameterStoreSecretsConfig.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.amazon.aws.partners.saasfactory.configuration;
+
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagement;
+import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagementClient;
+import com.amazonaws.services.simplesystemsmanagement.model.GetParameterRequest;
+import com.amazonaws.services.simplesystemsmanagement.model.GetParameterResult;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.PropertiesPropertySource;
+
+import java.util.Properties;
+
+/**
+ * This class will execute early as Spring boots up prior to the dispatcher
+ * servlet coming online. You must list any post processors in spring.factories
+ * in META-INF for it to be picked up.
+ * @author mibeard
+ */
+public class ParameterStoreSecretsConfig implements EnvironmentPostProcessor {
+
+    private static final Logger logger = LoggerFactory.getLogger(ParameterStoreSecretsConfig.class);
+
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        String awsRegion = environment.getProperty("AWS_REGION");
+
+        // This value is passed as the DB_PASS environment variable, but
+        // it's not really the password, it's the parameter name for SSM
+        String appPwParam = environment.getProperty("spring.datasource.password");
+
+        // Fetch the secret value for the application user database
+        // password from parameter store.
+        AWSSimpleSystemsManagement ssm = new AWSSimpleSystemsManagementClient();
+        ssm.setRegion(Region.getRegion(Regions.fromName(awsRegion)));
+        GetParameterRequest appPwRequest = new GetParameterRequest()
+                .withName(appPwParam)
+                .withWithDecryption(Boolean.TRUE);
+        GetParameterResult appPwResult = ssm.getParameter(appPwRequest);
+        String decryptedAppPassword = appPwResult.getParameter().getValue();
+
+        // Create properties with the same names as we launched Spring with
+        Properties decryptedProps = new Properties();
+        decryptedProps.put("spring.datasource.password", decryptedAppPassword);
+
+        // Now replace the existing environment variables with our new values
+        environment.getPropertySources().addFirst(new PropertiesPropertySource("myProps", decryptedProps));
+    }
+
+}
+
