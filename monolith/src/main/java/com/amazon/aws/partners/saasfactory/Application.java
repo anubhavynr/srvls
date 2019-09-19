fe0b49f3e0a2c7859c3efd9668660da2a372b2ce
Beardsley <mibeard@amazon.com>
2019-09-18 20:56:12 -0700

    Turning off the Spring ascii banner on start up

--- monolith/src/main/java/com/amazon/aws/partners/saasfactory/Application.java
+++ monolith/src/main/java/com/amazon/aws/partners/saasfactory/Application.java
@@ -1,19 +1,23 @@
 package com.amazon.aws.partners.saasfactory;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.ApplicationPidFileWriter;
-
-import java.io.File;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplicationBuilder app = new SpringApplicationBuilder(Application.class)
-				.web(WebApplicationType.SERVLET);
-		app.build().addListeners(new ApplicationPidFileWriter(new File("/home/ec2-user/monolith.pid")));
+				.web(WebApplicationType.SERVLET)
+				.bannerMode(Banner.Mode.OFF);
+
+		// We'll use the PID file to stop the app via CodeDeploy
+		// See scripts/stop_app.sh
+		app.build().addListeners(new ApplicationPidFileWriter("monolith.pid"));
+
 		app.run();
 	}
 
