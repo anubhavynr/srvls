34d64bc2eb6f44c563380abc167b9877d4fd7bbb
Trent <dgt@38f9d34e4108.ant.amazon.com>
2019-10-13 18:48:00 -0700

    Add product modal.

--- monolith/src/main/java/com/amazon/aws/partners/saasfactory/controller/ProductController.java
+++ monolith/src/main/java/com/amazon/aws/partners/saasfactory/controller/ProductController.java
@@ -6,19 +6,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import javax.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 @Controller
-public class ProductsController {
+public class ProductController {
 
 	@Autowired
 	private ProductService productService;
 
-	@GetMapping("/products")
-	public String products(Model model) throws Exception {
-		List<Product> products = productService.getProducts();
-		model.addAttribute("products", products);
-		return "products";
+	@GetMapping("/addnewproduct")
+	public String showAddNewProductForm(Product product) {
+		return "addProduct";
+	}
+
+	@PostMapping("/addProduct")
+	public String addProduct(@Valid Product product, Model model) throws Exception {
+		productService.saveProduct(product);
+		return "redirect:/";
 	}
 }
