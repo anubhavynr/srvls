82a2e8219b1a90ff538e90f963b4b22b54ab83fb
Trent <dgt@38f9d34e4108.ant.amazon.com>
2019-10-13 18:01:01 -0700

    Add product modal.

--- monolith/src/main/java/com/amazon/aws/partners/saasfactory/controller/ProductsController.java
+++ monolith/src/main/java/com/amazon/aws/partners/saasfactory/controller/ProductsController.java
@@ -6,9 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-// import javax.validation.Valid;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.validation.BindingResult;
+import javax.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -25,15 +24,12 @@ public class ProductsController {
 		return "products";
 	}
 
-	// @PostMapping("/addProduct")
-	// public String addProduct(@Valid Product product, BindingResult result, Model model) throws Exception {
-	// 	if (result.hasErrors()) {
-	// 		return "add-product";
-	// 	}
-
-	// 	productService.saveProduct(product);
-	// 	List<Product> products = productService.getProducts();
-	// 	model.addAttribute("products", products);
-	// 	return "products";
-	// }
+	@PostMapping("/addProduct")
+	public String addProduct(@Valid Product product, Model model) throws Exception {
+		productService.saveProduct(product);
+		List<Product> products = productService.getProducts();
+		// model.addAttribute("products", products);
+		// return "products";
+		return "redirect:/";
+	}
 }
