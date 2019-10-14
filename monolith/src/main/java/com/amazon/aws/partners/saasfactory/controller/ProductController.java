e8a1ade0a0c322515605d5344893a4dff1b21805
Trent <dgt@38f9d34e4108.ant.amazon.com>
2019-10-13 21:23:57 -0700

    Add product modal.

--- monolith/src/main/java/com/amazon/aws/partners/saasfactory/controller/ProductController.java
+++ monolith/src/main/java/com/amazon/aws/partners/saasfactory/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import javax.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
