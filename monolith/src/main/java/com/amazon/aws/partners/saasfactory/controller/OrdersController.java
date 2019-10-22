37e0e0f5ef914c29acd485676ed2a659ae9f327d
Trent <dgt@38f9d34e4108.ant.amazon.com>
2019-10-22 11:02:43 -0700

    Add order updates.

--- monolith/src/main/java/com/amazon/aws/partners/saasfactory/controller/OrdersController.java
+++ monolith/src/main/java/com/amazon/aws/partners/saasfactory/controller/OrdersController.java
@@ -1,7 +1,9 @@
 package com.amazon.aws.partners.saasfactory.controller;
 
 import com.amazon.aws.partners.saasfactory.domain.Order;
+import com.amazon.aws.partners.saasfactory.domain.Product;
 import com.amazon.aws.partners.saasfactory.service.OrderService;
+import com.amazon.aws.partners.saasfactory.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,10 +17,17 @@ public class OrdersController {
 	@Autowired
 	private OrderService orderService;
 
+	@Autowired
+	private ProductService productService;
+
 	@GetMapping("/orders")
 	public String orders(Model model) throws Exception {
 		List<Order> orders = orderService.getOrders();
+		List<Product> products = productService.getProducts();
+
 		model.addAttribute("orders", orders);
+		model.addAttribute("products", products);
+
 		return "orders";
 	}
 }
