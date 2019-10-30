0741d01c9d256f366951efa4d0cd5f2259c08d4b
Beardsley <mibeard@amazon.com>
2019-10-30 15:53:09 -0700

    Updating website deploy script to reflect new folder structure

--- website.sh
+++ website.sh
@@ -7,7 +7,7 @@ fi
 echo "Setting environment variables"
 MY_AWS_REGION=$(aws configure list | grep region | awk '{print $2}')
 echo "AWS Region = $MY_AWS_REGION"
-API_GATEWAY_URL=$(aws cloudformation describe-stacks | jq -r '.Stacks[] | select(.Outputs != null) | .Outputs[] | select(.OutputKey == "ApiGatewayEndpoint") | .OutputValue')
+API_GATEWAY_URL=$(aws cloudformation describe-stacks | jq -r '.Stacks[] | select(.Outputs != null) | .Outputs[] | select(.OutputKey == "ApiGatewayEndpointLab2") | .OutputValue')
 echo "API Gateway Invoke URL = $API_GATEWAY_URL"
 S3_WEBSITE_BUCKET=$(aws cloudformation describe-stacks | jq -r '.Stacks[] | select(.Outputs != null) | .Outputs[] | select(.OutputKey == "WebsiteS3Bucket") | .OutputValue')
 echo "S3 website bucket = $S3_WEBSITE_BUCKET"
@@ -18,7 +18,7 @@ echo
 # Edit src/shared/config.js in the ReactJS codebase
 # set base_url to the Serverless SaaS REST API stage v1 invoke URL
 echo "Configuring React to talk to API Gateway"
-cd /home/ec2-user/environment/saas-factory-serverless-workshop/Lab\ 2
+cd /home/ec2-user/environment/saas-factory-serverless-workshop/lab2/client
 sed -i -r -e 's|(^\s+)(base_url: )(process.env.REACT_APP_BASE_URL,)|//\1\2\3\n\1\2"'"${API_GATEWAY_URL}"'"|g' src/shared/config.js
 
 echo
