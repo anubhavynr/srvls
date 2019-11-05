eeea0a7fa19d9c02a4b07e0006d648669ec0209f
Beardsley <mibeard@amazon.com>
2019-11-05 11:16:59 -0800

    Remove Spring Security from lab 2 and replace the health check endpoint that we lost by removing the page controllers

--- lab2/server/scripts/start_app.sh
+++ lab2/server/scripts/start_app.sh
@@ -12,7 +12,7 @@ AWS_REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/doc
 DB_HOST=$(aws ssm get-parameters --region $AWS_REGION --names "${PREFIX}DB_HOST" | jq -r '.Parameters[0].Value')
 DB_NAME=$(aws ssm get-parameters --region $AWS_REGION --names "${PREFIX}DB_NAME" | jq -r '.Parameters[0].Value')
 DB_USER=$(aws ssm get-parameters --region $AWS_REGION --names "${PREFIX}DB_USER" | jq -r '.Parameters[0].Value')
-DB_PASS=$(aws ssm get-parameters --region $AWS_REGION --names "${PREFIX}DB_PASS" | jq -r '.Parameters[0].Value')
+DB_PASS="${PREFIX}DB_PASS"
 
 export AWS_REGION DB_HOST DB_NAME DB_USER DB_PASS
 
