ac5460fc790fb9587e39dffceaf1779bcd5c3d18
Beardsley <mibeard@amazon.com>
2019-11-07 15:40:24 -0800

    Too many backslashes in the CloudFormation parameter values

--- lab2.sh
+++ lab2.sh
@@ -11,14 +11,14 @@ echo "AWS Region = $MY_AWS_REGION"
 WORKSHOP_STACK=$(aws cloudformation describe-stacks | jq -r '.Stacks[] | select(.Outputs != null) | .Outputs[] | select(.OutputKey == "SaaSFactoryServerlessSaaSWorkshopStack") | .OutputValue')
 echo "Workshop stack = $WORKSHOP_STACK"
 
-#WORKSHOP_BUCKET=$(aws cloudformation describe-stacks | jq -r '[.Stacks[] | select(.Parameters != null) | .Parameters[] | select(.ParameterKey == "WorkshopS3Bucket")][0] | .ParameterValue')
 WORKSHOP_BUCKET=$(aws cloudformation describe-stacks | jq -r '.Stacks[] | select(.Outputs != null) | .Outputs[] | select(.OutputKey == "WorkshopBucket") | .OutputValue')
 echo "Workshop bucket = $WORKSHOP_BUCKET"
 
 LOAD_BALANCER_SG=$(aws cloudformation describe-stacks | jq -r '.Stacks[] | select(.Outputs != null) | .Outputs[] | select(.OutputKey == "LoadBalancerSecurityGroup") | .OutputValue')
-echo "Application load balancer DNS = $LOAD_BALANCER"
+echo "Application load balancer security group = $LOAD_BALANCER_SG"
 
 PUBLIC_SUBNETS=$(aws cloudformation describe-stacks | jq -r '.Stacks[] | select(.Outputs != null) | .Outputs[] | select(.OutputKey == "PublicSubnets") | .OutputValue')
+PUBLIC_SUBNETS=$(sed -e 's|,|\\,|' <<< $PUBLIC_SUBNETS)
 echo "Public subnets = $PUBLIC_SUBNETS"
 
 CODE_COMMIT_REPO=$(aws cloudformation describe-stacks | jq -r '.Stacks[] | select(.Outputs != null) | .Outputs[] | select(.OutputKey == "CodeCommitRepoName") | .OutputValue')
@@ -31,8 +31,8 @@ TEMPLATE_URL="https://${WORKSHOP_BUCKET}.s3-${MY_AWS_REGION}.amazonaws.com/lab2.
 echo "CloudFormation template URL = $TEMPLATE_URL"
 
 echo
-echo aws cloudformation create-stack --stack-name "${WORKSHOP_STACK}-lab2" --on-failure DO_NOTHING --capabilities CAPABILITY_NAMED_IAM --template-url "${TEMPLATE_URL}" --parameters \
-ParameterKey=LoadBalancerSecurityGroup,ParameterValue=\""${LOAD_BALANCER_SG}"\" \
+aws cloudformation create-stack --stack-name "${WORKSHOP_STACK}-lab2" --on-failure DO_NOTHING --capabilities CAPABILITY_NAMED_IAM --template-url "${TEMPLATE_URL}" --parameters \
+ParameterKey=LoadBalancerSecurityGroup,ParameterValue="${LOAD_BALANCER_SG}" \
 ParameterKey=LoadBalancerSubnets,ParameterValue="${PUBLIC_SUBNETS}" \
 ParameterKey=CodeCommitRepoName,ParameterValue="${CODE_COMMIT_REPO}" \
 ParameterKey=CodeCommitRepoURL,ParameterValue="${CODE_COMMIT_CLONE_URL}" \
